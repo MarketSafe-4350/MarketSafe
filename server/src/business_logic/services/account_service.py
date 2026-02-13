4f6f351c2b764f0d81ca302ed1af2ef2ea549978
FidelioC <fiocheah08@gmail.com>
2026-02-12 20:23:18 -0600

    FEATURE: add get request for account

--- server/src/business_logic/services/account_service.py
+++ server/src/business_logic/services/account_service.py
@@ -3,11 +3,14 @@ from src.api.errors.api_error import ApiError
 
 
 class AccountService:
-    def __init__(self): ...
+    def __init__(self):
+        # will have a DAO instance here
+        ...
 
     def create_account(
         self, email: str, password: str, fname: str, lname: str
     ) -> Account:
+        # should call DAO create account here
 
         # do logic here
         ##
@@ -21,3 +24,20 @@ class AccountService:
         )
 
         return account
+
+    def get_account_by_userid(self, userid: str) -> Account:
+        # should call DAO get account here
+
+        acc_test: Account = Account(
+            email="test1@gmail.com",
+            password="test",
+            fname="test",
+            lname="test",
+            account_id=1,
+            verified=False,
+        )
+        # do logic here (error input checking etc, if any)
+        ##
+
+        ##
+        return acc_test
--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from src.api.routes.account_routes import router as account_router
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="MarketSafe API")
+
+    # routers
+    app.include_router(account_router)
 
     return app
 
