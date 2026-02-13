e64023a51e47d0c4e82ba9b0151fc78b70291d96
michaelking <kingm4@myumanitoba.ca>
2026-02-13 15:21:47 -0700

    Basic security checks on account creation
    - Account aspects verified (email, password, first and last name)
    - uses utils/verification.py methods and self methods for verification

--- server/src/business_logic/services/account_service.py
+++ server/src/business_logic/services/account_service.py
@@ -1,6 +1,11 @@
 from server.src.api.errors import ApiError
 from server.src.domain_models import Account
+from server.src.utils import Validation
+from server.src.utils.errors import ValidationError
+import re
 
+PASSWORD_REGEX = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\s]{8,}$"
+ALLOWED_DOMAINS = ("umanitoba.ca", "myumanitoba.ca")
 
 class AccountService:
     """Service class for handling account-related business logic."""
@@ -23,8 +28,7 @@ class AccountService:
         Returns:
             Account: The newly created account domain model.
         """
-        # do logic here
-        ##
+        email, password, fname, lname = self.validate_account(email, password, fname, lname)
 
         # should call DAO create account here
 
@@ -37,6 +41,45 @@ class AccountService:
         )
 
         return account
+
+    def validate_account(self, email: str, password: str, fname: str, lname: str):
+        """ Validates created account information
+
+        Args: 
+            email (str): The email address for the new account - passed from create_account.
+            password (str): The password for the new account - passed from create_account.
+            fname (str): The first name of the account holder - passed from create_account.
+            lname (str): The last name of the account holder - passed from create_account.
+        
+        Returns:
+            email, password, fname, lname - All validated from utils/validation.py and self methods
+
+        Raises:
+            ValidationError: If any input fails validation checks.
+        """
+        email = Validation.valid_email(email)
+        self.validate_email(email)
+        
+        fname = Validation.require_str(fname, "First name")
+        lname = Validation.require_str(lname, "Last name")
+
+        Validation.not_empty(password, "Password")
+        self.validate_password(password)
+
+        return email, password, fname, lname
+    
+    def validate_email(self, email: str):
+        domain = email.split("@")[-1]
+        if domain not in ALLOWED_DOMAINS:
+            raise ValidationError("Email must be a valid University of Manitoba email address "
+                                    "(@umanitoba.ca / @myumanitoba.ca")
+
+    def validate_password(self, password: str):
+        if not re.fullmatch(PASSWORD_REGEX, password):
+            raise ValidationError(
+                "Password must be at least 8 characters long and include "
+                "1 uppercase, 1 lowercase, 1 number, and no spaces."
+            )
 
     def get_account_by_userid(self, userid: str) -> Account:
         """Retrieves an account by its user ID.
